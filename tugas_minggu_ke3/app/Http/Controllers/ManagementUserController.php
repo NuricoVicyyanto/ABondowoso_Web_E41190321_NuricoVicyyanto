571345680e2f9b92ca5811b02819b07e9326a219
NuricoVicyyanto <nuricovicyyanto2@gmail.com>
2021-03-23 15:04:21 +0700

    up

---
+++ tugas_minggu_ke3/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controlers;
+
+use Iluminate\Http\Request;
+
+class ManagementUserControler extends Controler
+{
+    public function index(){
+        return "Halo ini adalah method index, dalam controller ManagementUser";
+    }
+}
